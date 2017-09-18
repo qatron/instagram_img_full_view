dc51d11051c744c152fed353e4d433c830c2c995
qatron <patryk.st01@gmail.com>
2017-09-18 14:20:24 +0200

    Functions definition order reorganized.  ActiveArea:  remove by clicking, style changed

--- instagram_img_full_view.user.js
+++ instagram_img_full_view.user.js
@@ -28,9 +28,25 @@
 	}
 	
 	function NewImgBaseFind(){
-		var base=$('div[data-reactroot]').last();
+		//var base=$('div[data-reactroot]').last();
+		var base=$('div[data-reactroot]:last').children(':eq(1)').children(':eq(0)');
 		return base;
 	}
+	
+	var CustomViewer = {
+		imageSource: SourceImgFind().link,
+		CreateArea: function(){
+			var area=$('<div>',{
+				id: 'custom_viewer',
+				style: 'position: absolute; z-index:1001; width: 100vw; height: 100vh; background-color: black; opacity: 0.8;'
+			});
+			NewImgBaseFind().prepend(area);
+			$('#custom_viewer').click(function(){CustomViewer.RemoveArea();});
+		},
+		RemoveArea: function(){
+			$('#custom_viewer').remove();
+		}
+	};
 	
 	function ActiveArea(){
 		function NewImgActiveBaseFind(){
@@ -39,29 +55,17 @@
 		function ActiveAreaCreate(){
 			var area=$('<div>', {
 				id: 'custom_viewer_active_area',
-				style: 'position: absolute; width: 100%; height: 8%; top: 0; z-index: 999; opacity: 0; box-shadow: 0px 35px 20px -25px rgb(164, 205, 255) inset;'
+				style: 'position: absolute; width: 100%; height: 8%; top: 0; z-index: 1000; opacity: 0; box-shadow: 0px 35px 20px -25px rgb(164, 205, 255) inset;'
 				});
 			
 			if($('#react-root').eq(0).attr('aria-hidden')==='true'){
 				NewImgActiveBaseFind().prepend(area);
 			}
 			$('#custom_viewer_active_area').hover(function(){$(this).stop().fadeTo('fast', 0.8 );}, function(){$(this).stop().fadeTo('fast', 0 );});
+			$('#custom_viewer_active_area').click(function(){CustomViewer.CreateArea();});
 		}
 		ActiveAreaCreate();
 	}
-	
-	var CustomViewer = {
-		CreateArea: function(){
-			var area=$('<div>',{
-				id: 'custom_viewer',
-				style: ''
-			});
-			NewImgBaseFind().prepend(area);
-		},
-		RemoveArea: function(){
-			$('#custom_viewer').remove();
-		}
-	};
 	
 	AriaHiddenObserver(function(){
 		ActiveArea();
